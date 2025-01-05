7ed152e20a944bf48539b097bb0245965099d0bf
Gift <giftdavid101@gmail.com>
2025-01-05 18:01:50 +0100

    testing

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Server running")
+
+const express = require("express")
+const app = express()
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Welcome to farmers creek")
+
+})
+
+app.listen(3000, () => {
+    console.log("FarmCreek Server running")
+})
