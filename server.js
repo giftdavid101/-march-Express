8cd41f6014588243bc46a6aa9a7851ea7facae6d
Gift <giftdavid101@gmail.com>
2025-01-07 12:36:50 +0100

    connect app to database

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
 
 //routes
 app.get('/', (req, res) => {
-    res.send("Welcome to farmers creek")
+    res.send("Welcome to farmers creeks")
 
 })
 
-app.listen(3000, () => {
-    console.log("FarmCreek Server running")
+mongoose.set("strict", false)
+mongoose.connect(MONGO_DB_URI).then(() => {
+    console.log("Mongodb connected")
+    app.listen(3000, () => {
+        console.log("FarmCreek Server running")
+    })
+   
+}).catch((err) => {
+    console.log(err)
 })
