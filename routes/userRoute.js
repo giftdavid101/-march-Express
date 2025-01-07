a6bbee54ad54c41cddfae5c1e457bd5eaa34c25c
Gift <giftdavid101@gmail.com>
2025-01-07 13:27:40 +0100

    edit progress

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -13,14 +13,20 @@ router.post("/login",[
     body("email").notEmpty().isEmail().withMessage("A user must have a valid email"),
     body("password").notEmpty().isLength({ min: 8 }).withMessage( "A user must have a password. with a minimum of 8 characters")
 ])
+
 router.post("/forgotPassword", forgotPassword)
+
 router.post("/resetPassword/:token",[
     body("password").notEmpty().withMessage("Please provide your new password"),
     body("confirmPassword").notEmpty().withMessage( "Please confirm your new password "),
 ])
+
 router.post("/updatePassword",protect,[
 
     body("currentPassword").notEmpty().withMessage("Please provide your current password"),
     body("newPassword").notEmpty().withMessage("Please provide your new password"),
     body("confirmNewPassword").notEmpty().withMessage( "Please confirm your new password "),
 ])
+
+
+module.exports = router
