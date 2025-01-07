9f3f10318f54f4548935cd60082cf8c91b7fbccf
Gift <giftdavid101@gmail.com>
2025-01-07 13:14:22 +0100

    login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,3 +8,9 @@ router.post("/signup",[
     body("password").notEmpty().isLength({min: 8}).withMessage("Password is REQUIRED WITH A MINIMUM OF 8 CHARACTERS"),
     body("confirmPassword").notEmpty().isLength({min:8}).withMessage("confirm password")
 ])
+
+router.post("/login",[
+    body("email").notEmpty().isEmail().withMessage("A user must have a valid email"),
+    body("password").notEmpty().isLength({ min: 8 }).withMessage( "A user must have a password. with a minimum of 8 characters")
+])
+    
