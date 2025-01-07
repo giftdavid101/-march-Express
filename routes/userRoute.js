c9741cf5dc61438db0da033a57633858459785d9
Gift <giftdavid101@gmail.com>
2025-01-07 13:12:41 +0100

    create signup route

---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const router = express.Router()
+
+router.post("/signup",[
+    body("name").notEmpty().withMessage("A user must have a name"),
+    body("email").notEmpty().isEmail().withMessage("A user must have a valid email"),
+    body("password").notEmpty().isLength({min: 8}).withMessage("Password is REQUIRED WITH A MINIMUM OF 8 CHARACTERS"),
+    body("confirmPassword").notEmpty().isLength({min:8}).withMessage("confirm password")
+])
