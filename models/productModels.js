25848fb5e113ec7bee20b6885c8a8a49376dcf8f
Gift <giftdavid101@gmail.com>
2025-01-07 14:49:10 +0100

    productModel

---
+++ models/productModels.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A product must have a name'],
+    unique: [true, 'A product name must be unique']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A product must have a price']
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'A product must have quantity']
+  },
+  reservedQuantity: {
+    type: Number,
+    default: 0
+  },
+  description: {
+    type: String,
+  }
+})
+
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
+
