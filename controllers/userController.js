ba582a7750b11fa79dce3e912ecb326ba33394a8
Gift <giftdavid101@gmail.com>
2025-01-08 17:27:27 +0100

    createUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,4 +4,32 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto")
 
 const User = require("../models/userModels");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
+
+
+
+exports.createUser = catchAsync(async (req, res, next) => {
+    const errors = validationResult(req);
+  
+    if (!errors.isEmpty()) {
+      return next(new AppError(errors, 400));
+    }
+  
+    if (req.body.password !== req.body.confirmPassword) {
+      return next(
+        new AppError("Password and confirmPassword has to the same", 400),
+      );
+    }
+  
+    const body = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+  
+    const user = await User.create(body);
+  
+   
+  });
