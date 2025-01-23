ccd26b9064af080cd10aed6203b9a0a9f5f46eab
Gift <giftdavid101@gmail.com>
2025-01-23 02:27:21 +0100

    about

---
+++ FRONTEND/march-express/src/app/views/about/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function AboutPage() {
+
+
+    return (
+        <div className=" font-[family-name:var(--font-geist-sans)]">
+
+           About Ecommerce
+        </div>
+
+    );
+}
