fafdfd4e5bbadded3aa4943321d172fed68194c1
Gift <giftdavid101@gmail.com>
2025-01-23 02:52:07 +0100

    shop setup

---
+++ FRONTEND/march-express/src/app/shop/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+
+    return (
+        <div className=" font-[family-name:var(--font-geist-sans)]">
+
+            Home Ecommerce
+        </div>
+
+    );
+}
