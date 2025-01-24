bb46fe995b16ad8d93eb96eadffe1ba7d2825eec
Gift <giftdavid101@gmail.com>
2025-01-24 14:01:54 +0100

    add swagger doc

--- BACKEND/routes/userRoute.js
+++ BACKEND/routes/userRoute.js
@@ -5,12 +5,18 @@ const {createUser, loginUser, forgotPassword, resetPassword, updatePassword} = r
 const {protect} = require("../contollers/authController")
 const router = express.Router()
 
+
+
+
 router.post("/signup",[
     body("name").notEmpty().withMessage("A user must have a name"),
     body("email").notEmpty().isEmail().withMessage("A user must have a valid email"),
     body("password").notEmpty().isLength({min: 8}).withMessage("Password is REQUIRED WITH A MINIMUM OF 8 CHARACTERS"),
     body("confirmPassword").notEmpty().isLength({min:8}).withMessage("confirm password")
 ],createUser)
+
+
+
 
 router.post("/login",[
     body("email").notEmpty().isEmail().withMessage("A user must have a valid email"),
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,20 +1,38 @@
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
 require("dotenv").config()
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'My API Endpoints',
+            version: '1.0.0',
+            description: 'List of all API endpoints',
+        },
+    },
+    apis: ['./routes/*.js'], // Replace with the path to your route files
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// Swagger UI route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 
 //routes
 app.get('/', (req, res) => {
-    res.send("Welcome to farmers creeks")
+    res.send("Welcome to MarchEx")
 })
 
 mongoose.set("strict", false)
 mongoose.connect(process.env.MONGO_DB_URI).then(() => {
     console.log("Mongodb connected")
     app.listen(process.env.PORT || 3000, () => {
-        console.log("FarmCreek Server running")
+        console.log(" MarchEx Server running")
     })
    
 }).catch((err) => {
