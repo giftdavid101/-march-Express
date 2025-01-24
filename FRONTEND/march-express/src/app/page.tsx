570757ec4e76a5c6e759bc1235992ec81485819b
Gift <giftdavid101@gmail.com>
2025-01-24 12:12:08 +0100

    shop page

--- FRONTEND/march-express/src/app/page.tsx
+++ FRONTEND/march-express/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Navbar from "./components/layouts/header";
-import Footer from "./components/layouts/footer";
 import HomePage from "./home/page";
 
 export default function Home() {
@@ -7,9 +5,7 @@ export default function Home() {
 
   return (
     <div className=" font-[family-name:var(--font-geist-sans)]">
-        <Navbar/>
         <HomePage />
-        <Footer />
     </div>
   );
 }
--- FRONTEND/march-express/src/app/shop/page.tsx
+++ FRONTEND/march-express/src/app/shop/page.tsx
@@ -4,7 +4,7 @@ export default function ShopPage() {
     return (
         <div className=" font-[family-name:var(--font-geist-sans)]">
             <div className="container p-8">
-                <h1 className="text-3xl font-bold mb-6">Shop</h1>
+                <h6 className="text-2xl font-bold mb-6">Products</h6>
                 <div className="grid grid-cols-3 gap-8">
                     {/* Product Cards */}
                     {Array(9)
@@ -14,19 +14,22 @@ export default function ShopPage() {
                                 key={index}
                                 className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col items-center"
                             >
-                                <Image
-                                    src={`https://res.cloudinary.com/gift101/image/upload/v1737604230/pngegg_13_pmlqqr.png+${index + 1}`}
-                                    alt={`Product ${index + 1}`}
-                                    width={150}
-                                    height={150}
-                                    className="w-full object-cover"
-                                />
+                                <div className="bg-gray h-100p">
+                                    <Image
+                                        src="https://res.cloudinary.com/gift101/image/upload/v1737604230/pngegg_12_y3d4b3.png"
+                                        alt={`Product ${index + 1}`}
+                                        width={150}
+                                        height={150}
+                                        className="w-full object-cover"
+                                    />
+                                </div>
+
                                 <div className="p-4 w-full text-center">
                                     <h3 className="font-semibold text-lg mb-2">
                                         Product {index + 1}
                                     </h3>
                                     <p className="text-gray-500 text-sm">£20.00</p>
-                                    <button className="mt-4 px-6 py-2 bg-green-500 text-white rounded-lg">
+                                    <button className="mt-4 px-6 py-2 bg-teal-500 text-white rounded-lg">
                                         Add to Cart
                                     </button>
                                 </div>
