abe8192d151b5ec413091ab3c87d3703f5751ae4
Gift <giftdavid101@gmail.com>
2025-01-24 13:11:17 +0100

    login page

--- FRONTEND/march-express/src/app/login/page.tsx
+++ FRONTEND/march-express/src/app/login/page.tsx
@@ -1,10 +1,44 @@
-export default function LoginPage() {
+'use client'
+import "../css/signup.css"
+import {useState} from "react";
 
+export default function LoginPage() {
+    const [loading, setLoading] = useState(false);
 
     return (
         <div className=" font-[family-name:var(--font-geist-sans)]">
-
-            Login page
+            <div className="sign-up-style">
+            <div className="sign-up container">
+                <div className={'form-con fit'}>
+                    <form>
+                        <div className={'input-field'}>
+                            <label htmlFor={'email'}>Email Address</label>
+                            <input
+                                id={'email'}
+                                name={'email'}
+                                type={'email'}
+                            />
+                        </div>
+                        <div className={'input-field'}>
+                            <label htmlFor={'password'}>Password</label>
+                            <input
+                                id={'password'}
+                                name={'password'}
+                                type={'password'}
+                            />
+                        </div>
+                        <button className={"auth-btn"} type="submit" disabled={loading}>
+                            {loading ? <div className="loader"></div> : 'Log In'}
+                        </button>
+                    </form>
+                    <div>
+                        <p>
+                            Don't have an account? <a href={'/signup'}>Sign Up</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
 
     );
