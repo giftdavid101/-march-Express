c9c106873dc380bd8da3f1096ee7b9f312da21eb
Gift <giftdavid101@gmail.com>
2025-01-24 12:17:00 +0100

    signup page

---
+++ FRONTEND/march-express/src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+export default function SignUpPage() {
+
+
+    return (
+        <div className=" font-[family-name:var(--font-geist-sans)]">
+
+            Sign Up Ecommerce
+        </div>
+
+    );
+}
