c0ded1e1a5702651eebed445f3b17c49f399de8c
Gift <giftdavid101@gmail.com>
2025-02-06 14:06:29 +0100

    header.tsx

--- FRONTEND/march-express/src/app/components/layouts/header.tsx
+++ FRONTEND/march-express/src/app/components/layouts/header.tsx
@@ -1,56 +1,20 @@
-// 'use client';
-//
-//
-// import Link from 'next/link';
-// import { useState } from 'react';
-// import "../../css/nav.css"
-// const Navbar = () => {
-//     const [menuOpen, setMenuOpen] = useState(false);
-//
-//     return (
-//         <nav className="navbar">
-//             <div className="nav-con container">
-//                 <div className="navbar-brand">
-//                     <Link href="/">MarchEX</Link>
-//                     <button
-//                         className="menu-toggle"
-//                         onClick={() => setMenuOpen(!menuOpen)}
-//                         aria-label="Toggle menu"
-//                     >
-//                         ☰
-//                     </button>
-//                 </div>
-//                 <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
-//                     <li>
-//                         <Link href="/">Home</Link>
-//                     </li>
-//                     <li>
-//                         <Link href="/shop">Shop</Link>
-//                     </li>
-//                     <li>
-//                         <Link href="/login">Login</Link>
-//                     </li>
-//                     <li>
-//                         <Link href="/cart">Cart</Link>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// };
-//
-// export default Navbar;
 'use client';
 
 import Link from 'next/link';
 import { useState, useLayoutEffect } from 'react';
-import axios from 'axios';
+import { Modal } from 'antd';
 import "../../css/nav.css"
+import { useContext } from 'react';
+import { CartContext } from '../../createContext';
+import Image from "next/image";
+
 
 const Navbar = () => {
+    const { cartItems } = useContext(CartContext);
     const [menuOpen, setMenuOpen] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState({});
+    const [cartModalVisible, setCartModalVisible] = useState(false);
 
     useLayoutEffect(() => {
         const token = localStorage.getItem('token');
@@ -65,7 +29,7 @@ const Navbar = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('user');
         setIsLoggedIn(false);
-        setUser({}); // Reset user state
+        setUser({});
     };
 
     const getInitials = (name) => {
@@ -73,9 +37,21 @@ const Navbar = () => {
         return names[0][0] + names[names.length - 1][0];
     };
 
+    const handleCartClick = () => {
+        setCartModalVisible(true);
+    };
+
+    const handleCartModalOk = () => {
+        setCartModalVisible(false);
+    };
+
+    const handleCartModalCancel = () => {
+        setCartModalVisible(false);
+    };
+
     return (
         <nav className="navbar">
-            <div className="nav-con container">
+            < div className="nav-con container">
                 <div className="navbar-brand">
                     <Link href="/">MarchEX</Link>
                     <button
@@ -94,7 +70,10 @@ const Navbar = () => {
                         <Link href="/shop">Shop</Link>
                     </li>
                     <li>
-                        <Link href="/cart">Cart</Link>
+                        <button style={{display:'flex'}} onClick={handleCartClick}>
+                            <span>Cart</span>
+                            <span>({cartItems.length})</span>
+                        </button>
                     </li>
                     {isLoggedIn ? (
                         <div className="user flex items-center space-x-4">
@@ -121,8 +100,41 @@ const Navbar = () => {
 
                 </ul>
             </div>
+            <Modal
+                title="Your Cart"
+                open={cartModalVisible}
+                onOk={handleCartModalOk}
+                onCancel={handleCartModalCancel}
+            >
+                {cartItems.length === 0 ? (
+                    <span>Cart is empty!</span>
+                ) : (
+                    <div>
+                        {cartItems.map((item) => (
+                            <div style={{display:'flex', gap:8, marginTop:15}} key={item._id}>
+                                <div>
+                                    <Image
+                                        src={item.img}
+                                        alt={item.name}
+                                        width={60}
+                                        height={60}
+                                        className="h-full w-auto object-contain"
+                                    />
+                                </div>
+                                <div>
+                                    <b>{item.name}</b>
+                                    <p>Quantity: {item.quantity}</p>
+                                    <p>Price: ${item.price}</p>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </Modal>
         </nav>
     );
 };
 
 export default Navbar;
+
